56c6bbb3217f0146e61f6b119fae4bd892433569
聂建明 <541246>
2023-03-31 13:49:04 +0800

    默认key

--- src/components/SettingAction.tsx
+++ src/components/SettingAction.tsx
@@ -39,7 +39,7 @@ export default function SettingAction(props: {
           </SettingItem>
           <SettingItem icon="i-carbon:api" label="OpenAI Key">
             <input
-              type="password"
+              type="openaiAPIKey"
               value={props.setting().openaiAPIKey}
               class="max-w-150px ml-1em px-1 text-slate-7 dark:text-slate rounded-sm bg-slate bg-op-15 focus:bg-op-20 focus:ring-0 focus:outline-none"
               onInput={e => {
