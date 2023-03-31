8b5fcab782025eacd2aba85b179b1da56b789c87
聂建明 <541246>
2023-03-31 15:34:48 +0800

    a唉

--- src/system.ts
+++ src/system.ts
@@ -3,7 +3,7 @@ import type { Model } from "./types"
 export const defaultSetting = {
   continuousDialogue: true,
   archiveSession: false,
-  openaiAPIKey: "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",
+  openaiAPIKey: "",
   openaiAPITemperature: 60,
   password: "nie",
   systemRule: "",
